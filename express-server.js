51d575661b9df79a14dbd6d53757159fcb5f01b4
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-03 02:35:11 +0000

    adding todays tinyapp files

--- express-server.js
+++ express-server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
-var PORT = 8080;
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -22,6 +24,51 @@ app.get('/urls.json', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id]
+                     };
+  res.render('urls_show', templateVars);
+});
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
-})
+});
+
+app.post('/urls', (req, res) => {
+  let shortURL = generateRandomString();
+  let newLongURL = req.body.longURL;
+  console.log(newLongURL);
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
+function generateRandomString() {
+  let stringKey = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.ceil((Math.random() * stringKey.length));
+    randomString += stringKey[randomIndex - 1];
+  }
+  return randomString;
+}
+
+
+
+
+
+
+
+
+
+
